cd69fd0fb2ee5849c20503f57e967ba5b3dc9352
Alex <alexjbrown2@gmail.com>
2016-07-25 21:26:12 -0400

    add players

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -1,12 +1,12 @@
 <?php
   function db_connect(){
-    $conn = mysqli_connect('internal-db.s214596.gridserver.com', 'db214596', 'Childish2!');
+    $conn = mysql_connect('internal-db.s214596.gridserver.com', 'db214596', 'Childish2!');
     if (! $conn){
       die ("Could Not Connect:" . mysql_error());
     }
-    echo('it worked!')
+    echo 'it worked!';
   }
   function db_close(){
-    mysqli_close($conn);
+    mysql_close($conn);
   }
 ?>
