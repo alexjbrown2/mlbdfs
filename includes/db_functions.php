dcc05a52ee368b0e62cb5ab6928cc6ac8bb548f8
Alex Brown <albrown@verisign.com>
2016-07-31 17:02:25 -0400

    mysql work

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -1,12 +1,12 @@
 <?php
   function db_connect(){
-    $conn = mysql_connect('internal-db.s214596.gridserver.com', 'db214596', 'Childish2!');
+    $conn = mysqli_connect('internal-db.s214596.gridserver.com', 'db214596', 'Childish2!');
     if (! $conn){
       die ("Could Not Connect:" . mysql_error());
     }
     echo 'it worked!';
   }
   function db_close(){
-    mysql_close($conn);
+    mysqli_close($conn);
   }
 ?>
