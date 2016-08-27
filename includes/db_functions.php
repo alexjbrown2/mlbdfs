50fe4f98ed1462b9fcb04cd440d7d2e2b7c6e34f
Alex Brown <albrown@verisign.com>
2016-08-27 17:23:11 -0400

    ?

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -2,7 +2,7 @@
   function db_connect(){
     $conn = mysqli_connect('internal-db.s214596.gridserver.com', 'db214596', 'Childish2!', 'db214596_al3xbrown');
     if (!$conn){
-      die ("Could Not Connect:" . mysql_error());
+      die ("Could Not Connect:" . mysqli_error());
     }
     $GLOBALS['DBLink'] = $conn;
 
