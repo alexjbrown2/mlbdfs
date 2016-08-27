f98a0abdc25ef55c3af70f2ce65473e76c1af9e8
Alex Brown <albrown@verisign.com>
2016-08-27 16:30:15 -0400

    ?

--- includes.php
+++ includes.php
@@ -5,8 +5,8 @@
       //ini_set('display_startup_errors',1);
       //error_reporting(-1);
 
-	include("/mlbdfs/includes/config.php");
-	include("mlbdfs/includes/db_functions.php");
+	require_once("../includes/config.php");
+	include("includes/db_functions.php");
 
 
 ?>
